ec5d133754cc9bd9afbf4477b2ae50e3d9e175d9
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-07-29 14:01:58 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,24 +1,27 @@
-# EKS Cluster
 resource "aws_eks_cluster" "eks" {
   name     = "my-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+    subnet_ids = [
+      aws_subnet.public1.id,
+      aws_subnet.public2.id
+    ]
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_cluster_policy
   ]
 }
 
-# Node Group
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "example-node-group"
+  node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
-  subnet_ids      = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-
+  subnet_ids      = [
+    aws_subnet.public1.id,
+    aws_subnet.public2.id
+  ]
   scaling_config {
     desired_size = 2
     max_size     = 3
@@ -26,11 +29,10 @@ resource "aws_eks_node_group" "example" {
   }
 
   instance_types = ["t3.medium"]
-  ami_type       = "AL2_x86_64"
 
   depends_on = [
-    aws_iam_role_policy_attachment.eks_worker_node_policy,
-    aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.ec2_container_registry_read_only,
+    aws_iam_role_policy_attachment.worker_node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_read_only_policy
   ]
 }
