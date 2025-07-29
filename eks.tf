225f86509737b9a8e96e646f2f3466731e33a78d
Banita Patri <101166105+banita1985@users.noreply.github.com>
2025-07-29 14:13:54 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -4,8 +4,8 @@ resource "aws_eks_cluster" "eks" {
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.public1.id,
-      aws_subnet.public2.id
+      aws_subnet.public_subnet_1.id,
+      aws_subnet.public_subnet_2.id
     ]
   }
 
@@ -19,8 +19,8 @@ resource "aws_eks_node_group" "node_group" {
   node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
+    aws_subnet.public_subnet_1.id,
+   aws_subnet.public_subnet_2.id
   ]
   scaling_config {
     desired_size = 2
